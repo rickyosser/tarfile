39f084b844ddea2b1d7920acdad776e5b5836094
rickyosser <ricky@osser.se>
2024-02-14 22:40:37 +0100

    Create manifest.py

---
+++ manifest.py
@@ -0,0 +1,6 @@
+metadata(description="Read-only implementation of Python's tarfile.", version="0.0.1")
+
+# Originally written by Paul Sokolovsky.
+# Patches by Rickard Osser <rickard.osser@bluapp.com> ©2024 Bluapp AB
+
+package("circuitpython-tarfile")
